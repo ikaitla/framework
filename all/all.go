7f4c5e2bcd638ec847378f203576a62685a9bfb9
numerimondes <165125632+emyassine@users.noreply.github.com>
2026-02-04 16:40:37 +0100

    feat: add meta-package all to vendor full framework

---
+++ all/all.go
@@ -0,0 +1,21 @@
+// Package all pulls in the full Ikaitla framework surface area.
+// Import it for side-effects when you want to vendor all built-in packages.
+//
+// Example:
+//
+//	import _ "github.com/ikaitla/framework/all"
+package all
+
+import (
+	_ "github.com/ikaitla/framework/cli/ikaitla"
+	_ "github.com/ikaitla/framework/cli/shared"
+	_ "github.com/ikaitla/framework/profile"
+	_ "github.com/ikaitla/framework/ui"
+	_ "github.com/ikaitla/framework/ui/components"
+	_ "github.com/ikaitla/framework/ui/output"
+	_ "github.com/ikaitla/framework/ui/term"
+	_ "github.com/ikaitla/framework/ui/theme"
+)
+
+// Touch is a no-op to make the package feel intentional.
+func Touch() {}
